47be133f92d33a660762a351f1f4889c90a0215a
Dominick Bruno <dbruno@unseen.is>
2020-08-07 21:26:50 -0400

    Day 1 mvp.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,8 +6,46 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.running = False
+        self.sp = 7 #our stack pointer starts at the top of a 0-7 index
+        self.fl = 0b00000000 # all flags set to false on initialization
+        self.reg[self.sp] = 0xf4  # initialize stack pointer to RAM address f4
+
+    def call_stack(self, func):
+        branch_table = {
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,  
+            0b00000001: self.HLT
+        }
+        if func in branch_table:
+            branch_table[func]()
+        else:
+            print('invalid function')
+            sys.exit(1)
+
+    def LDI(self): #converts value to int
+        reg_num = self.ram_read(self.pc+1)
+        value = self.ram_read(self.pc+2)
+        self.reg[reg_num] = value
+        self.pc += 3 
+
+    def PRN(self): #prints to console
+        reg_num = self.ram_read(self.pc+1)
+        print(self.reg[reg_num])
+        self.pc += 2
+        
+    def HLT(self):
+        self.running = False
+        self.pc += 1
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MDR, MAR):
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +100,7 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.running = True
+        while self.running:
+            ir = self.ram[self.pc]
+            self.call_stack(ir) 
